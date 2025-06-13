948e8f006b45720d332a495c74a7801a12aaacfc
Arzario Irsyad Al Fatih <136824245+airsyadth@users.noreply.github.com>
2025-06-13 20:30:26 +0700

    Delete App.java

--- App.java
+++
@@ -1,9 +0,0 @@
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello, CI/CD World!");
-    }
-
-    public static int add(int a, int b) {
-        return a + b;
-    }
-}
