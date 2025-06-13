6d20aa45923ac9cfbcadb447c182a2342fefba6d
Arzario Irsyad Al Fatih <136824245+airsyadth@users.noreply.github.com>
2025-06-13 20:30:34 +0700

    Delete AppTest.java

--- AppTest.java
+++
@@ -1,9 +0,0 @@
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import org.junit.jupiter.api.Test;
-
-public class AppTest {
-    @Test
-    public void testAdd() {
-        assertEquals(7, App.add(3, 4));
-    }
-}
